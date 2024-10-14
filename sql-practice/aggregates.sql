d92db678256746b3e4a23e6e9f5a2fd011d66024
Srivatsan <vatsan1993@gmail.com>
2024-10-14 19:27:26 +0530

    finished bonus

--- sql-practice/aggregates.sql
+++ sql-practice/aggregates.sql
@@ -11,3 +11,25 @@ select name, min(birth_year) from cats;
 -- Write a query for the youngest cat, and the year it was born.
 
 select name, max(birth_year) from cats;
+
+-- bonus
+-- Write a query to list the number of toys per cat.
+select cats.name, count(*)
+from cats
+    join toys
+where
+    cats.id = toys.cat_id
+group by
+    cats.name;
+
+-- Write a query to determine which cats have been "spoiled" with two or more toys.
+
+select cats.name, count(*) as count
+from cats
+    join toys
+where
+    cats.id = toys.cat_id
+group by
+    cats.name
+having
+    count >= 2;
